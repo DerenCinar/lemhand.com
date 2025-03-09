5da1d26243633b083be33843eca0e38f67c6c392
Deren Cinar <108646259+DerenCinar@users.noreply.github.com>
2025-03-08 21:22:59 -0800

    Update app.js

--- public/app.js
+++ public/app.js
@@ -70,7 +70,7 @@ const app = Vue.createApp({
     methods: {
         resetCount() {
             if (localStorage.getItem("readArticles") !== this.articles.length) {
-                localStorage.setItem("readArticles", localStorage.getItem("readArticles") - 1);
+                localStorage.setItem("readArticles", localStorage.getItem("readArticles") + 1);
             }
         },
         onAuthEvent(user) {
